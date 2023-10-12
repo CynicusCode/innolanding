e92e16a17fdacaf21bec274a4d5c9cc9e5e4e028
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2023-10-11 22:16:48 -0600

    billing

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,21 +1,43 @@
-import { features } from '../constants';
-import styles, { layout } from '../style';
-import Button from './Button'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const Business = () => {
-  return (
-    <section id = 'features' className = "layout.section" > 
-      <div className = {layout.sectionInfo} >
-        <h2>
-        Get your email done faster and better. <br className='sm:block hidden '>
-        Your email sidekick.
-        </br> 
-        </h2>
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
-      </div>
+const Business = () =>  (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      InnoBox Ai Copilot is a cutting-edge solution that features, precise context understanding, response generation, and natural language processing.
+      </p>
 
-    </section>
-  )
-}
+      <Button styles={`mt-10`} />
+    </div>
 
-export default Business
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Business;
