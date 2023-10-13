3800401a95216335fe6ab1c8e1048be67f4e57b2
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2023-10-13 10:08:06 -0600

    project done

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>Meet Copilot: <br className='sm:block'/> Your AI Email Sidekick</h2>
+      <h2 className={styles.heading2}>Meet Inbotxer: <br className='sm:block'/> Your AI Email Sidekick</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Guided by cutting-edge technology, Copilot is designed to revolutionize your inbox experience with precision and efficiency.</p>
 
       <Button styles="mt-10" />
